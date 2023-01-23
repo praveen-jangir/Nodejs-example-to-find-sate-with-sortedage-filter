74365219782f5c4a0892a90ba275ebb85b634105
Praveen jangir <73075157+praveen-jangir@users.noreply.github.com>
2023-01-23 18:35:49 +0530

    forgot to update code

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ const users = [
  id: 3,
  name: 'Bob',
  email: 'bob@example.com',
- age: 25,
+ age: 35,
  city: 'New York',
  state: 'NY'
  }
 ];
 function getUsersInState(users, state){
-  if(state.length>2){
+  if(state.length>1){
     const result = users.filter(word => word.state == state);
-    const averageAge = parseInt(result.map(item => item.age).reduce((a, b) => a + b, 0)/result.length)
+    const averageAge = parseInt(Math.ceil(result.map(item => item.age).reduce((a, b) => a + b, 0)/result.length))
     result.sort((item1,item2) => (item1.age > item2.age) ? 1 : ((item2.age > item1.age) ? -1 : 0))
     result["averageAge"] = averageAge;
+    return result;
   }else{
     console.log("Please Enter Correct State");
     return null;
   }
-  return result;
 }
 console.log(getUsersInState(users, 'NY'));
