3bc132b882df9d92ffe1e128426aab342626bbec
Praveen jangir <73075157+praveen-jangir@users.noreply.github.com>
2023-01-23 17:32:16 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const users = [
+ {
+ id: 1,
+ name: 'John',
+ email: 'john@example.com',
+ age: 30,
+ city: 'New York',
+ state: 'NY'
+ },
+ {
+ id: 2,
+ name: 'Jane',
+ email: 'jane@example.com',
+ age: 25,
+ city: 'Chicago',
+ state: 'IL'
+ },
+ {
+ id: 3,
+ name: 'Bob',
+ email: 'bob@example.com',
+ age: 25,
+ city: 'New York',
+ state: 'NY'
+ }
+];
+function getUsersInState(users, state){
+  const result = users.filter(word => word.state == state);
+  const averageAge = result.map(item => item.age).reduce((a, b) => a + b, 0)/result.length
+  result.sort((item1,item2) => (item1.age > item2.age) ? 1 : ((item2.age > item1.age) ? -1 : 0))
+  result["averageAge"] = averageAge;
+  return result;
+}
+console.log(getUsersInState(users, 'NY'));
